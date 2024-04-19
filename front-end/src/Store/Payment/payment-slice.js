45a53974d7e5313419634d92ecee035a714aad73
kishandr15 <kishuhema@gmail.com>
2024-04-19 20:35:44 +0530

    first commit

---
+++ front-end/src/Store/Payment/payment-slice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit"; 
+
+const initialState = { 
+    paymentDetails: { checkinDate: null, checkoutDate: null, totalPrice: 0, propertyName: "", address: "", maximumGuest: 0, images: "", 
+},
+ }; 
+ const paymentSlice = createSlice({ 
+    name: "payment", initialState, reducers: { 
+        setPaymentDetails: (state, action) => { 
+            state.paymentDetails = action.payload; 
+        }, 
+    }, 
+}
+);
+export const { setPaymentDetails } = paymentSlice.actions; 
+export default paymentSlice;
